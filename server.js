37c65c6d7a314a9f8e33118c6c59b13ae36d7ffc
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2018-01-12 18:00:21 +0900

    talkのswitchを別ファイルに

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const fs = require("fs");
 const Formatter = require("./funcs/Formatter");
 const Dice = require("./funcs/Dice");
 const Janken = require("./funcs/Janken");
+const talk-fnc = require("./funcs/talk-fnc");
 
 let package = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
 let talk = JSON.parse(fs.readFileSync('./data/talk.json', 'utf8'));
@@ -26,102 +27,7 @@ let stream = mstdn.stream("streaming/user");
 			
 			switch (true) {
 				default:
-					switch (true){
-						default:
-							mstdn.post("statuses", {
-								status: [
-									`@${tootInfo.tooter}からVAWNへのメンションを確認しました。`,
-									"コマンドを正しく認識できなかったため処理が行えませんでした。申し訳ありません。",
-									"",
-									"現在VAWNが対応しているコマンドについては、以下を参照してください。",
-									"https://vawn.glitch.me/"
-								].join("\r\n"),
-						
-								visibility: "public",
-								in_reply_to_id: tootInfo.tootId
-							});
-
-							break;
-
-							case !!(variables = Formatter.htmlTextToPlainText(tootInfo.tootContent).match(/おは/)):
-								mstdn.post("statuses", {
-									status: [
-										`@${tootInfo.tooter}`,
-										talk.goodmorning[Math.floor(Math.random() * 2)]
-									].join("\r\n"),
-
-									visibility: "public",
-									in_reply_to_id: tootInfo.tootId
-								});
-
-								break;
-						
-							case !!(variables = Formatter.htmlTextToPlainText(tootInfo.tootContent).match(/おやす/)):
-								mstdn.post("statuses", {
-									status: [
-										`@${tootInfo.tooter}`,
-										talk.goodnight[Math.floor(Math.random() * 2)]
-									].join("\r\n"),
-
-									visibility: "public",
-									in_reply_to_id: tootInfo.tootId
-								});
-
-								break;
-
-							case !!(variables = Formatter.htmlTextToPlainText(tootInfo.tootContent).match(/あり/)):
-								mstdn.post("statuses", {
-									status: [
-										`@${tootInfo.tooter}`,
-										talk.thanks[Math.floor(Math.random() * 2)]
-									].join("\r\n"),
-
-									visibility: "public",
-									in_reply_to_id: tootInfo.tootId
-								});
-
-								break;
-								
-							case !!(variables = Formatter.htmlTextToPlainText(tootInfo.tootContent).match(/おめ/)):
-								mstdn.post("statuses", {
-									status: [
-										`@${tootInfo.tooter}`,
-										talk.congrats[Math.floor(Math.random() * 2)]
-									].join("\r\n"),
-
-									visibility: "public",
-									in_reply_to_id: tootInfo.tootId
-								});
-
-								break;
-								
-							case !!(variables = Formatter.htmlTextToPlainText(tootInfo.tootContent).match(/誕生日/)):
-								mstdn.post("statuses", {
-									status: [
-										`@${tootInfo.tooter}`,
-										talk.birthday[Math.floor(Math.random() * 2)]
-									].join("\r\n"),
-
-									visibility: "public",
-									in_reply_to_id: tootInfo.tootId
-								});
-
-								break;
-								
-							case !!(variables = Formatter.htmlTextToPlainText(tootInfo.tootContent).match(/バカ|ばか|馬鹿|アホ|あほ|ダメ|だめ|違う|バグ/)):
-								mstdn.post("statuses", {
-									status: [
-										`@${tootInfo.tooter}`,
-										talk.bad,
-										`#VAWN_improvement`
-									].join("\r\n"),
-
-									visibility: "public",
-									in_reply_to_id: tootInfo.tootId
-								});
-
-								break;
-					}
+					talk-fnc.talk-fnc;
 					break;
 
 				case !!(variables = Formatter.htmlTextToPlainText(tootInfo.tootContent).match(/(?:あなた|きみ|君|おまえ|お前|VAWN(?:| ))の(?:親|父親)/)):
