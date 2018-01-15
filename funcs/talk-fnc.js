21b7f0f23b25711874cf831b302be40e80130f40
Genbu Hase <hasegenbu@gmail.com>
2018-01-15 18:28:07 +0900

    herokuのURLに変更。

--- funcs/talk-fnc.js
+++ funcs/talk-fnc.js
@@ -12,7 +12,7 @@ module.exports = function talkFnc (mstdn, tootInfo) {
 					"コマンドを正しく認識できなかったため処理が行えませんでした。申し訳ありません。",
 					"",
 					"現在VAWNが対応しているコマンドについては、以下を参照してください。",
-					"https://vawn.glitch.me/"
+					"https://vawn.herokuapp.com/"
 				].join("\r\n"),
 
 				visibility: "public",
