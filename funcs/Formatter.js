38fed5d7bf70b4f8a06e76d58cb70c3bff1e8cc6
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2018-01-23 17:06:42 +0900

    スクレイピング結果の不要なテキストを削除

--- funcs/Formatter.js
+++ funcs/Formatter.js
@@ -15,4 +15,9 @@ module.exports = class Formatter {
 	static mentionRemove (htmlText = "") {
 		return htmlText.replace("@vawn [https://happy-oss.y-zu.org/@vawn] ", "");
 	}
+	static googleRemove (htmlText = "") {
+		return htmlText.replace("ウィキペディア", "");
+		return htmlText.replace("免責事項", "");
+		return htmlText.replace("さらに表示", "");
+	}
 }
