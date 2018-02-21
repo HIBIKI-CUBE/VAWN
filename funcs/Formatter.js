90006f639f7d934048c39bd7c9b547bb6aba13c4
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2018-02-21 23:07:47 +0900

    fix: mentionremoveを改善

--- funcs/Formatter.js
+++ funcs/Formatter.js
@@ -60,7 +60,7 @@ module.exports = class Formatter {
 	}
 
 	static mentionRemove (htmlText = "") {
-		return htmlText.replace("@vawn [https://happy-oss.y-zu.org/@vawn] ", "");
+		return htmlText.replace("@vawn", "").replace(/ \[([-_.!~*¥'()a-zA-Z0-9;¥/?:¥@&=+¥$,%#]+)\]/g, "");
 	}
 
 	static googleRemove (htmlText = "") {
