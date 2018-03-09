e291bfd83984ff8c5fa597433d5e3469e8037879
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2018-03-09 12:13:40 +0900

    進めた

--- IO/happy-oss.js
+++ IO/happy-oss.js
@@ -4,3 +4,19 @@ let mstdn = new Mastodon({
 	api_url: "https://happy-oss.y-zu.org/api/v1/",
 	access_token: process.env.YZU
 });
+
+module.export = class IO {
+	let stream = mstdn.stream("streaming/user");
+	stream.on("message", toot => {
+		if (toot.event == "notification" && toot.data.type == "mention") {
+			let tootInfo = Formatter.getInfoFromToot(toot);
+			let tootVis = tootInfo.tootVisibility;
+			let qna = new Object();
+
+			console.log(`${tootInfo.tooter} … ${tootInfo.tootContent}, ${tootVis}`);
+			
+			if (tootInfo.tootContent.toUpperCase().match(/@VAWN/g)) {
+			}
+		}
+	})
+}
