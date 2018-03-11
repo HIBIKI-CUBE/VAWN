29ab9179cd25a8a2c0ea161d1ef6805659c6158a
HIBIKI CUBE <hibiki.apple.1984@gmail.com>
2018-03-11 17:57:06 +0900

    add: (func).js追加など

--- funcs/talk-fnc.js
+++ funcs/talk-fnc.js
@@ -3,33 +3,13 @@ const fs = require("fs");
 
 const talk = JSON.parse(fs.readFileSync('./data/talk.json', 'utf8'));
 
-module.exports = function talkFnc (mstdn, tootInfo, tootVis) {
-	for (let i = 0; i < talk.notice.length; i++) {
-		if (tootInfo.tootContent.match(new RegExp(talk.notice[i]))) {
-			mstdn.post("statuses", {
-				status: [
-					`@${tootInfo.tooter}`,
-					talk.response[i][Math.floor(Math.random() * talk.response[i].length)]
-				].join("\r\n"),
-
-				visibility: tootVis,
-				in_reply_to_id: tootInfo.tootId
-			});
-
-			return;
+module.exports = class talkFnc {
+	static talk (tootInfo) {
+		
+		for (let i = 0; i < talk.notice.length; i++) {
+			if (tootInfo.tootContent.match(new RegExp(talk.notice[i]))) {
+				return talk.response[i][Math.floor(Math.random() * talk.response[i].length)]
+			}
 		}
 	}
-
-	mstdn.post("statuses", {
-		status: [
-			`@${tootInfo.tooter}からVAWNへのメンションを確認しました。`,
-			"コマンドを正しく認識できなかったため処理が行えませんでした。申し訳ありません。",
-			"",
-			"現在VAWNが対応しているコマンドについては、以下を参照してください。",
-			"https://vawn.herokuapp.com/"
-		].join("\r\n"),
-
-		visibility: tootVis,
-		in_reply_to_id: tootInfo.tootId
-	});
 }
