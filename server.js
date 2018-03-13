43013082dd22405cfb78f5f5ed44a2fa61ae261a
Genbu Hase <hasegenbu@gmail.com>
2018-03-13 16:24:52 +0900

    多数の修正 * Streamingクラス実装 * Notificationクラス実装 * Statusクラス実装

--- server.js
+++ server.js
@@ -5,21 +5,35 @@ const scrape = require('cheerio-httpcli');
 const webshot = require('webshot');
 const firebase = require("firebase-admin");
 
-const Formatter = require("./funcs/Formatter");
+const Status = require("./lib/Entities/Status");
+const Notification = require("./lib/Entities/Notification");
+const Streaming = require("./lib/Entities/Streaming");
+const Formatter = require("./lib/Formatter");
 
 const package = JSON.parse(fs.readFileSync("./package.json", "UTF-8"));
 
 let mstdn = new Mastodon({
-	api_url: "https://happy-oss.y-zu.org/api/v1/",
+	api_url: `${process.env.INSTANCE_YZU_OSS}/api/v1/`,
 	access_token: process.env.TOKEN_YZU_OSS
 });
 
 let stream = mstdn.stream("streaming/user");
-	stream.on("message", toot => {
-		if (toot.event == "notification" && toot.data.type == "mention") {
-			let tootInfo = Formatter.getInfoFromToot(toot);
+	stream.on("message", res => {
+		let streaming = new Streaming(res),
+			status = streaming.data.status;
 
-			console.log(`${tootInfo.tooter} … ${tootInfo.tootContent}, ${tootInfo.tootVisibility}`);
+		console.log(status);
+
+		if (status && status.isMention) {
+			console.log([
+				`<${status.actor}>`,
+				status.content,
+				`(Visibility: ${status.visibility})`
+			].join("\r\n"));
+
+			if (status.content.match(/\$debug eval (.*)/)) {
+				eval(console.log(status.content.match(/\$debug eval (.*)/)[1]));
+			}
 		}
 	});
 
@@ -32,7 +46,7 @@ let listener = app.listen(process.env.PORT, function () {
 	mstdn.post("statuses", {
 		status: [
 			`VAWN Next is ready.`,
-			`Version: ${packageInfo.version}`
+			`Version: ${package.version}`
 		].join("\r\n"),
 
 		visibility: "unlisted"
